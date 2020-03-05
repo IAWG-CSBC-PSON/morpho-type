757ba91d0eece034020d3a9fd064d20e405485e1
Artem Sokolov <artem.sokolov@gmail.com>
2020-03-05 12:14:46 -0600

    Wrangle (#4)
    * Added local caches and plot outputs
    
    * Data files with unnormalized probabilities

--- sokolov/wrangle.R
+++ sokolov/wrangle.R
@@ -20,11 +20,22 @@ fGeom <- c("Area", "Eccentricity", "Solidity", "Extent", "EulerNumber",
 fNeig <- str_c("Neighbor_", 1:5)
 fPos  <- c("X_position", "Y_position")
 
-## Identify, download, and load all source feature files
-Xraw <- c(Lung1 = "syn19035296",
-          Lung2 = "syn19035616",
-          Lung3 = "syn19035696") %>% map(syn) %>%
-    map( read_csv, col_types=cols(CellID=col_integer()) )
+## Identify and download all source feature files
+fn <- c(Lung1 = "syn19035296",
+        Lung2 = "syn19035616",
+        Lung3 = "syn19035696") %>% map(syn)
+
+## Local cache
+if( FALSE )
+{
+    fn <- c(Lung1 = "LUNG-1-LN_40X_master.csv",
+            Lung2 = "LUNG-2-BR_40X_master.csv",
+            Lung3 = "LUNG-3-PR_40X_master.csv") %>%
+        map( partial(here, "data","raw") )
+}
+
+## Load all downloaded / local files
+Xraw <- map( fn, read_csv, col_types=cols(CellID=col_integer()) )
 
 ## Define quantiles (QQ) for outlier removal
 QQ <- list(
@@ -37,6 +48,15 @@ f <- function(X, Q) map(fExpr, ~naivestates::GMMfit(X, CellID, .x, qq=Q[[.x]]))
 stopifnot( identical(names(QQ), names(Xraw)) )
 Fits <- map2(Xraw, QQ, f) %>% map(bind_rows)
 
+## Plot selected fits
+if( FALSE )
+{
+    P <- tidyr::crossing( Tissue=names(Fits), Marker=fExpr ) %>%
+        mutate( fnOut = str_c(Tissue, "-", Marker, ".png"),
+               Plot = map2(Tissue, Marker, ~naivestates::plotFit(Fits[[.x]], .y)) )
+    with( P, walk2(fnOut, Plot, ggsave) )
+}
+
 ## Marker -> Cell type (mct) association
 mct <- c("Immune" = "CD45", "Stroma" = "ASMA", "Tumor" = "KERATIN")
 ct <- names(mct)                   ## Cell types: Immune, Stroma, Tumor
@@ -48,7 +68,7 @@ fcc <- function(F) {
         mutate(Sum    = pmap_dbl(!!ect, sum),
                Argmax = pmap_int(!!ect, lift_vd(which.max)),
                Label  =  map_chr(Argmax, ~ct[.x])) %>%
-        mutate_at( ct, ~.x/Sum ) %>%
+#        mutate_at( ct, ~.x/Sum ) %>%
         select(CellID, Label, one_of(ct))
 }
 
@@ -61,4 +81,4 @@ Data <-map( Xraw, select, CellID, one_of(c(fGeom, fNeig, fPos)) ) %>%
     map2( Labels, ., inner_join, by="CellID" )
 
 ## Save to files (assign to tmp to suppress stdout)
-tmp <- imap( Data, ~write_csv(.x, here("data",str_c(.y,".csv.gz"))) )
+tmp <- imap( Data, ~write_csv(.x, here("data",str_c(.y,"-rawprob.csv"))) )
